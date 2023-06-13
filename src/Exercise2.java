49d6f0eea47d548f69ba878f8e964a88a83c728e
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-06-13 09:24:55 +0200

    add ex2

---
+++ src/Exercise2.java
@@ -0,0 +1,8 @@
+//What’s an alternative way you could add 2 + 4 without using “+” symbol?
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        System.out.println(2 | 4);
+        System.out.println(2 + 4);
+    }
+}
