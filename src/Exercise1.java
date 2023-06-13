5eaff22c995c118b345174427c9222ce75cc34a4
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-06-13 09:13:48 +0200

    init repo

---
+++ src/Exercise1.java
@@ -0,0 +1,11 @@
+// If you could receive up to eight different types of radio signals simultaneously (into your
+// computer & a program you wrote) and you needed to be able to record which of those eight
+// you received at any given time, what would be a highly compact (or compressed - using
+// the least amount of memory or storage) way to record them (using what you learned in this
+// module)? For example, you could receive signals A + B + C simultaneously, or A + D, or just
+// G, etc.
+//    1. If you received signals A + D + C simultaneously, how could you efficiently determine
+//    that D was one of the signals you received - based on your solution above?
+
+public class Exercise1 {
+}
